671af86b42b8ebf89b6a15ebdd3e0a96432d099e
luctivud on VM/Administrator <luctivud@gmail.com>
2022-08-22 11:15:18 +0000

    send more verbose response of click to controller

--- TicTacToeGame/TTTGameManager.cpp
+++ TicTacToeGame/TTTGameManager.cpp
@@ -12,19 +12,24 @@ TTTGameManager::~TTTGameManager()
 	delete m_pTTTModel;
 }
 
-const std::pair<int, int> TTTGameManager::responseToClick(const int xPos, const int yPos)
+const std::array<int, 3> TTTGameManager::responseToClick(const int xPos, const int yPos)
 {
+	std::array<int, 3> responseArray = {0, -1, -1};
 	std::pair<int, int> boxClicked = m_pTTTView->checkIfClickOnBoard(xPos, yPos);
-	if (boxClicked.first != -1 and boxClicked.second != -1 and m_pTTTModel->isValidMove(boxClicked)) // clicked on Board
+	if ((boxClicked.first != -1) and (boxClicked.second != -1) and 
+		(m_pTTTModel->isValidMove(boxClicked))) // click was on Board
 	{
 		m_pTTTModel->updateTurn();
 		m_pTTTModel->updateBoard(boxClicked);
 		m_pTTTView->updateBoard(boxClicked, m_pTTTModel->getTurn());
-		m_pTTTModel->checkIfGameEnded(boxClicked);
+		responseArray[0] = m_pTTTModel->checkIfGameEnded(boxClicked);
 	}
 	else
 	{
 		boxClicked = { -1, -1 };
 	}
-	return boxClicked;
+
+	responseArray[1] = boxClicked.first;
+	responseArray[2] = boxClicked.second;
+	return responseArray;
 }
--- TicTacToeGame/TTTGameManager.h
+++ TicTacToeGame/TTTGameManager.h
@@ -2,13 +2,14 @@
 
 #include "TTTModel.h"
 #include "TTTView.h"
+#include <array>
 
 class TTTGameManager
 {
 public:
 	TTTGameManager(HWND hwnd);
 	virtual ~TTTGameManager();
-	const std::pair<int, int> responseToClick(const int xPos, const int yPos);
+	const std::array<int, 3> responseToClick(const int xPos, const int yPos);
 private:
 	HWND m_pHWnd;
 	TTTModel* m_pTTTModel;
